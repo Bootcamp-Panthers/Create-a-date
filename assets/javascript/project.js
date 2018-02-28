31aa13a2083287a4965fde99d448fd8ceb3ab0b5
Nick <nickkwon92@gmail.com>
2018-02-28 10:06:03 -0800

    added zomato ajax call to project.js

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -17,3 +17,37 @@ $.ajax({
     var momentString = momentObj.format('dddd, MMMM Do YYYY, h:mm:ss a');
     console.log(momentString);
 });
+
+
+//#submitt is the submit button id
+$("#submitt").on("click", function(event){
+    //#locationz is the form for the location
+    var locate = $("#locationz").val();
+    console.log(locate);
+    zomatoCITY();
+    });
+    
+    
+    function zomatoCITY(){
+        var locate = $("#locationz").val();
+        $.ajax({
+            url: "https://developers.zomato.com/api/v2.1/locations?query=" + locate,
+            type: "GET",
+            dataType: "json",
+            headers: {"user-key": "f01cb7831e9ebde3e857af1190a5f819"}
+        }).then(function (response){
+            console.log(response);
+            var cityNum = response.location_suggestions[0].city_id;
+            $.ajax({
+                url: "https://developers.zomato.com/api/v2.1/search?entity_id=" + cityNum + "&entity_type=city",
+                type: "GET",
+                dataType: "json",
+                headers: {"user-key": "f01cb7831e9ebde3e857af1190a5f819"}
+        
+            }).then(function (response2){
+                console.log(response2)
+            });
+    
+        });
+    }
+    
